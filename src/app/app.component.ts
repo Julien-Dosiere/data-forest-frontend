bb4c18cc9d7766f58838d5db9310e83189faf2fd
Julien-Dosiere <juliendosiere@yahoo.fr>
2021-09-13 00:40:23 +0200

    http client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'data-forest-frontend';
+
+  constructor(private dataService: DataService) {
+  }
+
+  ngOnInit() {
+    this.dataService.fetchData();
+  }
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Subject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  error = new Subject();
 
-  constructor() { }
+  apiURL = 'https://jd-data-forest.herokuapp.com/forests/'
+  constructor(private httpClient: HttpClient) { }
+
+  fetchData() {
+    console.log('fetching..');
+    const datas = this.httpClient
+      .get<any>(
+      this.apiURL,
+    )
+      .subscribe(responseData => console.log(responseData), error => {
+        console.log('error !');
+
+        this.error.next(error.message);
+    });
+    //console.log(datas);
+  }
 }
